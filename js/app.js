8b3ba6aafd2a8b97270734689d621fde74a4ec21
haidv <duongvanhai2712@gmail.com>
2024-06-06 06:11:40 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+import { util } from './util.js';
+import { theme } from './theme.js';
+import { audio } from './audio.js';
+import { progress } from './progress.js';
+
+window.util = util;
+window.theme = theme;
+window.audio = audio;
+window.progress = progress;
